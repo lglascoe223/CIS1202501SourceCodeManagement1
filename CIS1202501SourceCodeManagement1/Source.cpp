767d406d407b74e990222ef89ffa9a11ea84688c
Leslie Glascoe <leslie.glascoe@sinclair.edu>
2021-03-11 14:05:33 -0500

    First Comment

---
+++ CIS1202501SourceCodeManagement1/Source.cpp
@@ -0,0 +1,17 @@
+/*
+ * Leslie Glascoe
+ * CIS 1202-501
+ * 3/11/2021
+ * Source Code Management
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello World" << endl;
+	
+	return 0;
+}
